ca9d854d7ded9378f3a1e07fccb3fbc1e1f26e5a
vextor <victor.casas@tecsup.edu.pe>
2026-06-10 16:24:26 -0500

    efeto de movimiento listo

--- Assets/Scripts/PlayerControlet.cs
+++ Assets/Scripts/PlayerControlet.cs
@@ -2,19 +2,33 @@ using UnityEngine;
 
 public class PlayerControlet : MonoBehaviour
 {
+    [SerializeField] private float speed = 80f;
 
-[SerializeField] private float speed = 80f;
+    private SpriteRenderer spriteRenderer;
+    private bool facingRight = true;
+
     void Start()
     {
-
-    
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
 
     void Update()
     {
-        float velocidadX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        Vector3 position = transform.position;
-        transform.position = new Vector3(velocidadX + position.x, position.y, position.z);
+        float movimiento = Input.GetAxis("Horizontal");
+
+        // Movimiento
+        transform.position += Vector3.right * movimiento * speed * Time.deltaTime;
+
+        // Giro visual
+        if (movimiento < 0 && facingRight)
+        {
+            spriteRenderer.flipX = true;
+            facingRight = false;
+        }
+        else if (movimiento > 0 && !facingRight)
+        {
+            spriteRenderer.flipX = false;
+            facingRight = true;
+        }
     }
 }
