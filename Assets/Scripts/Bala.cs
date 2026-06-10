f4f9340999a74c0511a3e2f009b77b16651d9829
vextor <victor.casas@tecsup.edu.pe>
2026-06-10 16:16:48 -0500

    cambios 1

--- Assets/Scripts/Bala.cs
+++ Assets/Scripts/Bala.cs
@@ -5,12 +5,12 @@ public class Bala : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.up * 10 * Time.deltaTime;
     }
 }
